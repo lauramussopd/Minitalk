48d7f72eeebda57c4bfe0112f21ba9caafa9bb3b
Laura Musso <laurmuss@car15s5.42barcelona.com>
2023-08-23 18:40:50 +0200

    wed640

--- client.c
+++ client.c
@@ -1,14 +1,46 @@
 # include "ft_libft/libft.h"
 # include <stdio.h>
+#include <signal.h>
+
+void send_binary(char *str, int pidserv)
+{
+	int num_bit;
+	int i;
+
+	num_bit = 0;
+	i = 0;
+	while(str[i])
+	{
+		while (num_bit < 8)
+		{
+			if (((str[i]) & (128 >> num_bit)) == 0)
+			{
+				ft_printf("0");
+				kill(pidserv, SIGUSR2);
+			}
+			else
+			{
+				ft_printf("1");
+				kill(pidserv, SIGUSR1);
+			}
+		usleep(500);
+		num_bit++;
+		}
+		num_bit = 0;
+		i++;
+	}
+}
+	
 
 int	main(int argc, char **argv)
 {
 	int	pidserv;
 
-//  if (argc != 3)
-//		return (-1);
+  	if (argc != 3)
+		return (-1);
 	pidserv = ft_atoi(argv[1]);
-    ft_printf("mi pid:%d", pidserv);
-	//conv_bin(argv[2], pidserv);
+    ft_printf("mi pid:%d\n", pidserv);
+	send_binary(argv[2], pidserv);
+
 	return (0);
 }
--- server.c
+++ server.c
@@ -10,10 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_libft/libft.h"
+#include "ft_libft/libft.h"
+#include <signal.h>
+
+void receive_bit(int sign)
+{
+    static int  num_bit = 0;
+    static char c = 0;
+
+   
+
+    if (sign == SIGUSR1)
+        c = c | (128 >> num_bit);
+    num_bit++;
+    if (num_bit == 8)
+    {
+        ft_printf("%c", c);
+        num_bit = 0;
+        c = 0;
+    }
+}
 
 int main()
 {
     ft_printf("%d\n", getpid());
-
+	signal(SIGUSR1, receive_bit);
+	signal(SIGUSR2, receive_bit);
+	while (1)
+		pause();
+	return (0);
 }
